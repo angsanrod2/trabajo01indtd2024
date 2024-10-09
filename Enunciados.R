a146cc302bbb6533430b72323724028e4a45d59e
angsanrod2 <angelasaro19@gmail.com>
2024-10-09 13:11:27 +0200

    enunciados de los problemas

---
+++ Enunciados.R
@@ -0,0 +1,23 @@
+##Problema 1 -------------------------------------------------------------------
+#Aplicar los criterios de decisión bajo incertidumbre a los problemas cuya matriz 
+#de valores numéricos viene dada en la tabla siguiente:
+
+####### e1  ## e2  ## e3 |
+-------------------------
+  #d1: |  40  |  25   |  5 |
+  #d2: |  50  |  15   |  0 |
+  #d3: |  30  |  30   |  10|
+  -------------------------
+  
+  
+  ##Problema 2 -------------------------------------------------------------------
+#Una empresa de marketing digital está evaluando tres posibles estrategias publicitarias
+#para una nueva campaña. Dependiendo de la reacción del mercado (positivo, moderado
+#o negativo), los ingresos proyectados (en miles de euros) son los siguientes:
+#Publicidad en redes sociales: 100, 50, 1
+#Publicidad en televisión: 80, 60, 30
+#Publicidad en radio: 60, 40, 20 
+
+#¿Cuál sería la opción que usted le recomendaría a la empresa?
+
+
